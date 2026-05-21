259a9c95a8ec06d5d6a97697a084b11f0bded3f6
Gabriel Venerusso <98406849+gabrielvenerusso@users.noreply.github.com>
2026-05-21 19:30:58 -0300

    Atualizar o extract_pedido.py

--- src/triggers/extract_pedido.py
+++ src/triggers/extract_pedido.py
@@ -1,19 +1,47 @@
 import azure.functions as func
 import logging
+import pyodbc
 import os
-#from orchestrators.etl_orchestrator import ETLOrchestrator
 
 app = func.Blueprint()
 
-
 @app.timer_trigger(schedule="0 0 6 * * *", arg_name="timer", run_on_startup=False)
 def extract_pedido(timer: func.TimerRequest) -> None:
+    
     sql_server = os.getenv("SQL_SERVER_SOURCE")
     sql_database = os.getenv("SQL_DATABASE_SOURCE")
     sql_user = os.getenv("SQL_USER_SOURCE")
-    sql_password = os.getenv("SQL_PASSWORD_SOURCE")
+    sql_pass = os.getenv("SQL_PASSWORD_SOURCE")
 
-    logging.info("Iniciando a extração de categoria_produto")
-    logging.info(f"Conectando ao banco de dados SQL Server: {sql_server}")
-    logging.info(f"Banco de dados: {sql_database}")
-    logging.info("Extração de categoria_produto concluída com sucesso")
+    # Configura a string de conexão para o banco de dados SQL Server
+    conn_str = (
+        "DRIVER={ODBC Driver 18 for SQL Server};"
+        f"SERVER={sql_server};"
+        f"DATABASE={sql_database};"
+        f"UID={sql_user};"
+        f"PWD={sql_pass};"
+        "Encrypt=yes;"
+        "TrustServerCertificate=no;"
+        "Connection Timeout=30;"
+    )
+
+   
+    try:
+        # Estabelece a conexão com o banco de dados usando pyodbc
+        with pyodbc.connect(conn_str) as conn:
+            # Cria um cursor para executar a consulta   
+            cursor = conn.cursor()
+            
+            query = "select top 5 * from erp.pedido"
+
+            # Executa a consulta SQL
+            cursor.execute(query)
+
+            # Busca todos os resultados da consulta
+            rows = cursor.fetchall()
+    
+            logging.info(rows)           
+
+    except Exception as e:
+        logging.error(f"Erro ao ler erp.pedido: {str(e)}")
+        raise
