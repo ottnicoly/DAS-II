9f366c92636f7d35b5fc01cadfd8e8b4300b56ad
gabriel <gabrielmevenerusso@gmail.com>
2026-06-10 20:13:51 -0300

    Arrumada function da ultima aba.

--- src/triggers/extract_estoque_saldo.py
+++ src/triggers/extract_estoque_saldo.py
@@ -1,5 +1,9 @@
-from etl_utils import copiar_tabela
+import azure.functions as func
+from .etl_utils import copiar_tabela
+
+app = func.Blueprint()
 
 
-def executar(source_conn, target_conn):
-    copiar_tabela(source_conn, target_conn, "estoque_saldo")
+@app.timer_trigger(schedule="0 6 6 * * *", arg_name="timer", run_on_startup=False)
+def extract_estoque_saldo(timer: func.TimerRequest) -> None:
+    copiar_tabela("estoque_saldo")
